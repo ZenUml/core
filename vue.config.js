6a693ff659e6407050916f60d6086fd0faf81623
PengXiao <eagle.xiao@gmail.com>
2020-04-28 20:28:57 +1000

    Use svg-url-loader for the User icon

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  chainWebpack: config => config.resolve.symlinks(false)
+  chainWebpack: config =>{
+    // A workaround that allows npm link or yarn link
+    // https://cli.vuejs.org/guide/troubleshooting.html#symbolic-links-in-node-modules
+    config.resolve.symlinks(false)
+
+    // We need to clear the pre-built svg rule
+    // use `vue-cli-service inspect to check the webpack
+    // the built-in webpack uses file-loader
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear();
+        svgRule
+          //.test(/\.svg$/)  // this is not required because the built-in svg rule already defined `test`
+          .use('svg-url-loader')
+          .loader('svg-url-loader')
+          .end()
+
+  }
 }
