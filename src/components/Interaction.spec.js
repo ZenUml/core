dc90d63de8d89b219c122b342df44788ec05c57e
PengXiao <eagle.xiao@gmail.com>
2020-05-03 20:39:04 +1000

    Test re-written with given centerOf participants.

--- src/components/Interaction.spec.js
+++ src/components/Interaction.spec.js
@@ -1,24 +1,28 @@
 import {createLocalVue, shallowMount} from '@vue/test-utils'
 import Vuex from 'vuex'
 import Interaction from './Interaction'
+import { Store } from '../index'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
 describe('Interaction width', () => {
   test.each([
-    [1, 100, 99],
-    [-1, 100, 101],
-    [1, -100, 101],
-    [-1, -100, 99]
-  ])('If offset is %s and distance is %s, interactionWidth should be %s', (offset, distance, width) => {
-    const store = new Vuex.Store({
-      getters: {
-        distance: () => () => distance
-      }
-    })
+    // A --- ?px ---> B
+    [ 1,  10, 25, 14],
+    [ 1,  25, 10, 16],
+    [-1,  10, 25, 16],
+    [-1,  25, 10, 14],
+  ])('If offset is %s and distance is %s, interactionWidth should be %s', (offset, a, b, width) => {
+    Interaction.computed.to = () => 'B';
+    Store.getters.centerOf = () => (participant) => {
+      if(participant === 'A') return a
+      if(participant === 'B') return b
+    };
+    const store = new Vuex.Store(Store)
     const wrapper = shallowMount(Interaction, {
       store, localVue, propsData: {
+        from: 'A',
         offset: offset,
       }
     });
