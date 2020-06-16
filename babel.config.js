2c5be191bfbd55151096fdfaeffef0a4722f50e6
PengXiao <eagle.xiao@gmail.com>
2020-06-16 21:30:28 +1000

    Removed babel config that is causing issues.

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,6 @@
 module.exports = {
   presets: [
-    '@vue/app',
-    [
-      "@babel/env",
-      {
-        "targets": {
-          "edge": "17",
-          "firefox": "60",
-          "chrome": "67",
-          "safari": "11.1",
-        },
-        "useBuiltIns": "usage",
-        "corejs": "3.6.4",
-      }
-    ]
+    '@vue/app'
   ],
   plugins: [
     "@babel/plugin-transform-modules-commonjs",
