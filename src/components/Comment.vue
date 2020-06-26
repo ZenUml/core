70bb28625ecbebed2f3788263059b98dec7ded43
PengXiao <eagle.xiao@gmail.com>
2020-06-26 22:32:59 +1000

    Added support of markdown in comments.

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="comments" v-html="comment"></div>
+  <div class="comments" v-html="markedComment"></div>
 </template>
 
 <script type="text/babel">
+  import marked from 'marked'
+
   export default {
     name: 'comment',
-    props: ['comment']
+    props: ['comment'],
+    computed: {
+      markedComment() {
+        return marked(this.comment)
+      }
+    }
   }
 </script>
