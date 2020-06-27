01e89498a13fb5638f745a59be7ccf95cadb1a2e
PengXiao <eagle.xiao@gmail.com>
2020-06-27 15:33:09 +1000

    Fixed a defect that erroring when the code is not an endpoint.

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -19,7 +19,7 @@
       const endpointPattern = /(GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH)\s+(.+)/ig
       // let found = code.match(endpointPattern)
       let found = endpointPattern.exec(code)
-      if (found.length === 3) {
+      if (found?.length === 3) {
         return `
           <code class="rest-api">
           <span class="http-method-${found[1].toLowerCase()}">${found[1]}</span>
--- src/demo1.js
+++ src/demo1.js
@@ -6,45 +6,8 @@
 // Nested method
 // Assignment
 export default `
-@Starter(Client)
-// \`POST /orders/\`
-// 
-// \`GET /orders/:id\`
-// 
-// \`OPTIONS /orders/:id\`
-// 
-// All possibilities \`POST /order/:id\`
-// 1. application/json
-// 1. text/text
-// 
-// 
-// |a|b|
-// |-|-|
-// |c|d|
-// |e|f|
-// |g|h|
-// 
-// 
-// \`\`\`javascript
-// for(int i = 0; i < 5; i++) {
-//   doSomething()
-//   return: [
-//     {action1, confirm: encrypted}
-//     {action2, confirm: encrypted}
-//     {action3, confirm: encrypted}
-//   ]
-// }
-// \`\`\`
-AnotherService.lodgeBlockCardAction() {
-  // GET /cards + JWT
-  // 1. Door 1, blocked
-  // 1. Door 2, inactive
-  DoorService.getList()
-  
-  //- [ ] To be implemented
-  //- [x] Already done
-  //
-  //
-  buildCarousel()
-}
+// \`POST order\`
+// Make sure \`a\`
+OrderService.create()
 `
+
