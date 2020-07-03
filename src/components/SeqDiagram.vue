d6fc2e2fe65b4f3a010668dd1bf1efa376d9ad67
PengXiao <eagle.xiao@gmail.com>
2020-07-03 20:21:03 +1000

    Use setTime to dispatch code.

--- src/components/SeqDiagram.vue
+++ src/components/SeqDiagram.vue
@@ -25,25 +25,30 @@
       MessageLayer
     },
     mounted() {
-      // Callback function to execute when mutations are observed
-      const callback = function(mutationsList) {
-        // Use traditional 'for loops' for IE 11
-        for(let mutation of mutationsList) {
-          if (mutation.type === 'childList') {
-            store.dispatch('updateCode', { code: mutation.addedNodes[0]?.textContent || 'Example.method'})
-          }
-        }
-      }
-
-      // Create an observer instance linked to the callback function
-      const observer = new MutationObserver(callback);
-
-      // Start observing the target node for configured mutations
-      // The slot is added after $nextTick. Chaining next tick does not work.
-      const targetNode = this.$refs['dsl'];
-      // Options for the observer (which mutations to observe)
-      const config = { childList: true};
-      observer.observe(targetNode, config);
+      const that = this
+      // Looks like we have to use setTimeout. The approach below does not work on confluence.
+      setTimeout(function () {
+        store.dispatch('updateCode', { code: that.$slots.default[0].text || 'Example.method'})
+      })
+      // // Callback function to execute when mutations are observed
+      // const callback = function(mutationsList) {
+      //   // Use traditional 'for loops' for IE 11
+      //   for(let mutation of mutationsList) {
+      //     if (mutation.type === 'childList') {
+      //       store.dispatch('updateCode', { code: mutation.addedNodes[0]?.textContent || 'Example.method'})
+      //     }
+      //   }
+      // }
+      //
+      // // Create an observer instance linked to the callback function
+      // const observer = new MutationObserver(callback);
+      //
+      // // Start observing the target node for configured mutations
+      // // The slot is added after $nextTick. Chaining next tick does not work.
+      // const targetNode = this.$refs['dsl'];
+      // // Options for the observer (which mutations to observe)
+      // const config = { childList: true};
+      // observer.observe(targetNode, config);
     }
   }
 </script>
