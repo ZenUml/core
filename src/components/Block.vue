4f3426dafdd3feed73b4a6c9b630aa8653160a9f
PengXiao <eagle.xiao@gmail.com>
2020-10-16 16:28:07 +1100

    Add animation to the diagram.

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <div v-for="(stat, index) in statements" :key="index">
-      <statement :context="stat" :from="from" :offset="offset"/>
-    </div>
+    <transition name="fade">
+      <div v-for="(stat, index) in statements" :key="index">
+        <statement :context="stat" :from="from" :offset="offset"/>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -22,3 +24,27 @@
     }
   }
 </script>
+
+<style>
+  .fade-enter-active, .fade-leave-active {
+    animation: bounce-in .3s;
+  }
+
+  .fade-enter, .fade-leave-to {
+    animation: bounce-in .3s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+
+    50% {
+      transform: scale(1.1);
+    }
+
+    100% {
+      transform: scale(1);
+    }
+  }
+</style>
