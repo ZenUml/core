f78e836ae54fdf5e515cde82a2150d6d0be08cb4
MrCoder <eagle.xiao@gmail.com>
2021-01-17 18:59:53 +1100

    Changed Store to a function. So we do not need deep clone anymore.

--- src/demo2.js
+++ src/demo2.js
@@ -2,18 +2,4 @@
 // Right to left
 // RTL in if
 // RTL in while
-export default `@Starter(ZenUML) RET ret = A.methodA() {
-  B.methodB() {
-    A.methodA()
-  }
-  if (X) {
-    C.methodC() {
-      a = A.methodA()
-    }
-  }
-  while (Y) {
-    C.methodC() {
-      A.methodA()
-    }
-  }
-}`
+export default `Demo2`
