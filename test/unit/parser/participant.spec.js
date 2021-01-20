5fafd52934bfeecabe193b7be107916220bee4d2
MrCoder <eagle.xiao@gmail.com>
2021-01-20 16:35:46 +1100

    Added test for two participants in one line.

--- test/unit/parser/participant.spec.js
+++ test/unit/parser/participant.spec.js
@@ -6,6 +6,13 @@ test('A participant - A', () => {
   expectText(participants[0]).toBe('A')
 })
 
+test('A participant - A B', () => {
+  let rootContext = seqDsl.RootContext('A B');
+  let participants = rootContext.participant();
+  expectText(participants[0]).toBe('A')
+  expectText(participants[1]).toBe('B')
+})
+
 test('A participant - A 100', () => {
   let rootContext = seqDsl.RootContext('A 100');
   let participants = rootContext.participant();
