2f5dcf8c265f57360b0c84c697964a3665c9e771
MrCoder <eagle.xiao@gmail.com>
2021-01-20 15:53:52 +1100

    Added test for two lines of comment.

--- test/unit/parser/comment.spec.js
+++ test/unit/parser/comment.spec.js
@@ -1,5 +1,19 @@
 // This test file covers scenarios when comments are used
 let seqDsl = require('../../../src/parser/index');
+test('one line of comment', () => {
+  let rootContext = seqDsl.RootContext('//C1\nA.m');
+  expect(rootContext).not.toBeNull()
+  expect(rootContext.block().stat().length).toBe(1)
+  expect(rootContext.block().stat()[0].getComment()).toBe('//C1\n')
+})
+
+test('two lines of comment', () => {
+  let rootContext = seqDsl.RootContext('//C1\n//C2\nA.m');
+  expect(rootContext).not.toBeNull()
+  expect(rootContext.block().stat().length).toBe(1)
+  expect(rootContext.block().stat()[0].getComment()).toBe('//C1\n//C2\n')
+})
+
 test('comment only block is valid', () => {
     let rootContext = seqDsl.RootContext('A.method() { // comment \n }');
     expect(rootContext).not.toBeNull()
