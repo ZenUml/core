4b886bf5e9c0b7d862e877e8bf35db3783ce2cee
PengXiao <eagle.xiao@gmail.com>
2021-02-07 14:54:31 +1100

    Added test for nested message.

--- src/parser/CodeRange.spec.ts
+++ src/parser/CodeRange.spec.ts
@@ -14,4 +14,15 @@ describe('CodeRange', () => {
     expect(codeRange.start).toBe(0);
     expect(codeRange.stop).toBe(3);
   });
+
+  it('should have an start and an end', () => {
+    const m1 = getMessageContext('A.m1{B.m2}');
+    const codeRange1 = CodeRange.from(m1);
+    expect(codeRange1.start).toBe(0);
+    expect(codeRange1.stop).toBe(10);
+    const m2 = m1.braceBlock().block().stat()[0].message();
+    const codeRange2 = CodeRange.from(m2);
+    expect(codeRange2.start).toBe(5);
+    expect(codeRange2.stop).toBe(9);
+  });
 });
