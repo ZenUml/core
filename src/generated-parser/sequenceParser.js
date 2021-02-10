d8c9c9e71c042d2cc594761a6f176789d7492c67
PengXiao <eagle.xiao@gmail.com>
2021-02-10 13:45:39 +1100

    updated yarn antlr* command

--- src/generated-parser/sequenceParser.js
+++ src/generated-parser/sequenceParser.js
@@ -1,4 +1,4 @@
-// Generated from ./sequenceParser.g4 by ANTLR 4.8
+// Generated from sequenceParser.g4 by ANTLR 4.8
 // jshint ignore: start
 var antlr4 = require('antlr4/index');
 var sequenceParserListener = require('./sequenceParserListener').sequenceParserListener;
