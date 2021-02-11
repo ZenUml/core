6f7c4d57a2c066ac085e65ed60d3b6d555944780
PengXiao <eagle.xiao@gmail.com>
2021-02-11 18:21:15 +1100

    Extracted Assignee method.

--- src/parser/Owner.js
+++ src/parser/Owner.js
@@ -3,8 +3,14 @@ const sequenceParser = require('../generated-parser/sequenceParser')
 
 const seqParser = sequenceParser.sequenceParser;
 const CreationContext = seqParser.CreationContext;
+CreationContext.prototype.Assignee = function () {
+  return this.creationBody()?.assignment()?.assignee()?.getText();
+}
+/**
+ * @return {string}
+ */
 CreationContext.prototype.Owner = function () {
-  const assignee = this.creationBody()?.assignment()?.assignee()?.getText() || '';
+  const assignee = this.Assignee() || '';
   const type = this.creationBody()?.construct()?.getText();
   if (!type) {
     return 'Missing Constructor'
@@ -15,6 +21,9 @@ CreationContext.prototype.Owner = function () {
 
 const MessageContext = seqParser.MessageContext
 
+/**
+ * @return {string}
+ */
 MessageContext.prototype.Owner = function () {
   if (this.messageBody().func()?.to()) {
     let participant = this.messageBody().func().to().getText();
