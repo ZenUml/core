dce33952b70329b5dbd476a1f7eaf6aaf682f147
PengXiao <eagle.xiao@gmail.com>
2021-02-28 08:27:59 +1100

    If no stat exists, the first participant is treated as the starter. This is to allow the first participant to occupy starter's position when we list participants.

--- src/parser/index.js
+++ src/parser/index.js
@@ -68,14 +68,35 @@ RetContext.prototype.getReturnTo = function() {
 ProgContext.prototype.Starter = function () {
   const declaredStarter = this.head()?.starterExp()?.starter()?.getTextWithoutQuotes()
   let starterFromStartingMessage;
+  let starterFromParticipant;
+  let starterFromParticipantGroup;
   const stat = this.block()?.stat();
   if (stat && stat[0]) {
     const messageFrom = stat[0].message()?.messageBody()?.from()?.getTextWithoutQuotes()
     const asyncMessageFrom = stat[0].asyncMessage()?.from()?.getTextWithoutQuotes()
     starterFromStartingMessage = messageFrom || asyncMessageFrom
+  } else {
+
+    const children = this.head()?.children;
+    if(children && children[0]) {
+      const child = children[0];
+      if (child instanceof sequenceParser.sequenceParser.ParticipantContext) {
+        starterFromParticipant = child.name().getTextWithoutQuotes();
+      }
+      if (child instanceof sequenceParser.sequenceParser.GroupContext) {
+        const participants = child.participant();
+        if(participants && participants[0]) {
+          starterFromParticipantGroup = participants[0].name()?.getTextWithoutQuotes();
+        }
+      }
+    }
   }
 
-  return declaredStarter || starterFromStartingMessage || '_STARTER_'
+  return declaredStarter ||
+    starterFromStartingMessage ||
+    starterFromParticipant ||
+    starterFromParticipantGroup ||
+    '_STARTER_'
 }
 
 antlr4.ParserRuleContext.prototype.getTextWithoutQuotes = function() {
