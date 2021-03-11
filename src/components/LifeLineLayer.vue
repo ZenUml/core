20062442a9d6154103e9c6f323ec77a017e9653a
MrCoder <eagle.xiao@gmail.com>
2021-03-11 11:35:55 +1100

    Set min-width for lifeline layer and message layer to 375.
    The width for iPhone 6/7/8 simulator on Chrome.

--- src/components/LifeLineLayer.vue
+++ src/components/LifeLineLayer.vue
@@ -72,8 +72,8 @@
 <style scoped>
   .life-line-layer {
     display: flex;        /* This is to remove spaces between inline-blocks, not necessary for generated and non-formatted code*/
-    min-width: 500px;     /* This line and the next line make them wide enough when there are ONLY two participants */
-    justify-content: space-evenly;
+    min-width: 375px;     /* This line and the next line make them wide enough when there are ONLY two participants */
+    justify-content: space-between; /* makes it easier to calculate the width of the message layer */
     white-space: nowrap;  /* Do not wrap to a new line */
     position: absolute;   /* So that message layer is overlaid. */
     height: 100%;         /* To give height to the line */
