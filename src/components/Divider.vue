eba74a70f2faca48e44fb72a7ef1106ddd0ab122
MrCoder <eagle.xiao@gmail.com>
2021-03-11 18:14:53 +1100

    Remove double quote from divider name.

--- src/components/Divider.vue
+++ src/components/Divider.vue
@@ -27,7 +27,7 @@
         return this.centerOf(this.from)
       },
       name: function () {
-        return this.context.divider().name().getText()
+        return this.context.divider().name().getTextWithoutQuotes()
       }
     }
   }
