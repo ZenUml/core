2ddfbd8a6cb97a879d40d49e50ea729870dd4760
MrCoder <eagle.xiao@gmail.com>
2021-03-16 21:55:28 +1100

    Extracted ProgContext for Starter function.

--- src/parser/index.js
+++ src/parser/index.js
@@ -5,6 +5,7 @@ const ToCollector = require('./ToCollector');
 const ChildFragmentDetector = require('./ChildFragmentDetecotr');
 require('./IsCurrent');
 require('./Owner');
+require('./ProgContext');
 require('./RetContext');
 require('./StatContext');
 
@@ -22,43 +23,6 @@ function rootContext(code) {
   const parser = new sequenceParser.sequenceParser(tokens);
   parser.addErrorListener(new SeqErrorListener());
   return parser._syntaxErrors ? null : parser.prog();
-}
-
-const seqParser = sequenceParser.sequenceParser;
-const ProgContext = seqParser.ProgContext;
-
-ProgContext.prototype.Starter = function () {
-  const declaredStarter = this.head()?.starterExp()?.starter()?.getTextWithoutQuotes()
-  let starterFromStartingMessage;
-  let starterFromParticipant;
-  let starterFromParticipantGroup;
-  const stat = this.block()?.stat();
-  if (stat && stat[0]) {
-    const messageFrom = stat[0].message()?.messageBody()?.from()?.getTextWithoutQuotes()
-    const asyncMessageFrom = stat[0].asyncMessage()?.from()?.getTextWithoutQuotes()
-    starterFromStartingMessage = messageFrom || asyncMessageFrom
-  } else {
-
-    const children = this.head()?.children;
-    if(children && children[0]) {
-      const child = children[0];
-      if (child instanceof sequenceParser.sequenceParser.ParticipantContext) {
-        starterFromParticipant = child.name()?.getTextWithoutQuotes();
-      }
-      if (child instanceof sequenceParser.sequenceParser.GroupContext) {
-        const participants = child.participant();
-        if(participants && participants[0]) {
-          starterFromParticipantGroup = participants[0].name()?.getTextWithoutQuotes();
-        }
-      }
-    }
-  }
-
-  return declaredStarter ||
-    starterFromStartingMessage ||
-    starterFromParticipant ||
-    starterFromParticipantGroup ||
-    '_STARTER_'
 }
 
 antlr4.ParserRuleContext.prototype.getTextWithoutQuotes = function() {
