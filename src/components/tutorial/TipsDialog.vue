e9696bbe5c989e0a9dc623b2cc87e2038cb14322
MrCoder <eagle.xiao@gmail.com>
2022-01-17 10:58:23 +1100

    Renamed "ZenUML Help" to "ZenUML Tips"

--- src/components/tutorial/TipsDialog.vue
+++ src/components/tutorial/TipsDialog.vue
@@ -10,7 +10,7 @@
       <div>
         <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6" slot="header">
           <h3 class="text-xl leading-6 font-medium text-gray-900 inline-block">
-            ZenUML Help
+            ZenUML Tips
           </h3>
           <button type="button" @click="showModal = false" class="float-right bg-white rounded-md inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
             <span class="sr-only">Close menu</span>
