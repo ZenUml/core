839c6d66688d61f4bf403267aa9391a407edc742
MrCoder <eagle.xiao@gmail.com>
2022-01-16 21:22:03 +1100

    Removed some comments.

--- src/components/DiagramFrame.vue
+++ src/components/DiagramFrame.vue
@@ -11,34 +11,13 @@
     <div>
       <a target="_blank" href="https://zenuml.com" class="float-right clearfix text-xs">ZenUML.com</a>
       <button id="show-modal" @click="showModal=true">Show Modal</button>
-      <!-- This example requires Tailwind CSS v2.0+ -->
       <div v-show="showModal" class="fixed z-40 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-          <!--
-            Background overlay, show/hide based on modal state.
-
-            Entering: "ease-out duration-300"
-              From: "opacity-0"
-              To: "opacity-100"
-            Leaving: "ease-in duration-200"
-              From: "opacity-100"
-              To: "opacity-0"
-          -->
           <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 
           <!-- This element is to trick the browser into centering the modal contents. -->
           <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
-          <!--
-            Modal panel, show/hide based on modal state.
-
-            Entering: "ease-out duration-300"
-              From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              To: "opacity-100 translate-y-0 sm:scale-100"
-            Leaving: "ease-in duration-200"
-              From: "opacity-100 translate-y-0 sm:scale-100"
-              To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          -->
           <div class="z-40 inline-block align-bottom bg-white rounded-lg px-4 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:p-2">
             <div>
               <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6" slot="header">
