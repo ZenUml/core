00b82639a9e0a56700204d3f2460c601d1e7222a
MrCoder <eagle.xiao@gmail.com>
2022-01-16 18:03:58 +1100

    Can control the modal.

--- src/components/DiagramFrame.vue
+++ src/components/DiagramFrame.vue
@@ -11,6 +11,10 @@
     <seq-diagram/>
     <div>
       <a target="_blank" href="https://zenuml.com" class="float-right clearfix text-xs">ZenUML.com</a>
+      <button id="show-modal" @click="showModal=true">Show Modal</button>
+      <modal v-if="showModal" @close="showModal = false">
+        <h3 slot="header">Custom header</h3>
+      </modal>
     </div>
   </div>
 </template>
@@ -19,8 +23,14 @@
 import {mapGetters} from "vuex";
 import DiagramTitle from "@/components/DiagramTitle";
 import SeqDiagram from "@/components/SeqDiagram";
+import Modal from "@/components/tutorial/Modal";
 export default {
   name: "DiagramFrame",
+  data() {
+    return {
+      showModal: false
+    }
+  },
   computed: {
     ...mapGetters(['rootContext']),
     title() {
@@ -31,6 +41,7 @@ export default {
     }
   },
   components: {
+    Modal,
     DiagramTitle,
     SeqDiagram
   },
@@ -48,5 +59,6 @@ export default {
   margin-bottom: 4px;
   padding: 4px;
 }
+
 
 </style>
