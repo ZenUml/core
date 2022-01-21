65de04c32f8e507214efa0dbcc6c7a2515329430
MrCoder <eagle.xiao@gmail.com>
2022-01-21 17:22:35 +1100

    Show participant on occurrence for debugging.

--- src/components/Occurrence.vue
+++ src/components/Occurrence.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="occurrence" data-el-type="occurrence" :data-belongs-to="participant" :data-x-offset="center" :data-debug-center-of="computedCenter">
+    {{participant}}
     <block v-if="this.context.braceBlock()"
            :context="context.braceBlock().block()"
            :selfCallIndent="selfCallIndent"
