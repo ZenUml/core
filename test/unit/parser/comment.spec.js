25ad02aa8817ea6a7ba7b3d3de1c0baf15565871
MrCoder <eagle.xiao@gmail.com>
2022-01-26 15:40:41 +1100

    Use Fixture.firstStatement in comment.spec.js

--- test/unit/parser/comment.spec.js
+++ test/unit/parser/comment.spec.js
@@ -1,35 +1,21 @@
-// This test file covers scenarios when comments are used
-let seqDsl = require('../../../src/parser/index');
+const {Fixture} = require("./fixture/Fixture");
 test('one line of comment', () => {
-  let rootContext = seqDsl.RootContext('//C1\nA.m');
-  expect(rootContext).not.toBeNull()
-  expect(rootContext.block().stat().length).toBe(1)
-  expect(rootContext.block().stat()[0].getComment()).toBe('C1\n')
+  expect(Fixture.firstStatement('//C1\nA.m').getComment()).toBe('C1\n')
 })
 
 test('two lines of comment', () => {
-  let rootContext = seqDsl.RootContext('//C1\n//C2\nA.m');
-  expect(rootContext).not.toBeNull()
-  expect(rootContext.block().stat().length).toBe(1)
-  expect(rootContext.block().stat()[0].getComment()).toBe('C1\nC2\n')
+  expect(Fixture.firstStatement('//C1\n//C2\nA.m').getComment()).toBe('C1\nC2\n')
 })
 
 test('comment only block is valid', () => {
-    let rootContext = seqDsl.RootContext('A.method() { // comment \n }');
-    expect(rootContext).not.toBeNull()
-    expect(rootContext.block().stat().length).toBe(1)
-  let message = rootContext.block().stat()[0]
+  let message = Fixture.firstStatement('A.method() { // comment \n }')
     .message();
   let braceBlock = message.braceBlock();
   expect(braceBlock.getComment()).toBe(' comment \n')
 })
 
 test('comment after method call is valid', () => {
-    let rootContext = seqDsl.RootContext('A.method() { internal() \n// comment \n }');
-    expect(rootContext).not.toBeNull()
-    expect(rootContext.block().stat().length).toBe(1)
-
-  let message = rootContext.block().stat()[0]
+  let message = Fixture.firstStatement('A.method() { internal() \n// comment \n }')
     .message();
   let braceBlock = message.braceBlock();
   expect(braceBlock.getComment()).toBe(' comment \n')
