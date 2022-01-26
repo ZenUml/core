add252e9a39b383426fbb1e6221c304565f6a720
MrCoder <eagle.xiao@gmail.com>
2022-01-26 16:04:01 +1100

    Fixed an issue - AsyncMessageContext did not implement SignatureText function.

--- src/parser/SignatureText.ts
+++ src/parser/SignatureText.ts
@@ -4,10 +4,15 @@ const sequenceParser = require('../generated-parser/sequenceParser')
 
 const seqParser = sequenceParser.sequenceParser;
 const MessageContext = seqParser.MessageContext;
+const AsyncMessageContext = seqParser.AsyncMessageContext;
 const CreationContext = seqParser.CreationContext;
 
 MessageContext.prototype.SignatureText = function () {
   return this.messageBody()?.func()?.signature()?.map((s: any) => s?.getTextWithoutQuotes()).join('.');
+}
+
+AsyncMessageContext.prototype.SignatureText = function () {
+  return this.content()?.getTextWithoutQuotes()
 }
 
 CreationContext.prototype.SignatureText = function () {
