587384fad2a7765fac8b8a0ad331d97e708f0618
MrCoder <eagle.xiao@gmail.com>
2022-01-26 15:16:28 +1100

    Use Fixture.firstStatement in simple-message.spec.js

--- test/unit/parser/simpe-message.spec.js
+++ test/unit/parser/simpe-message.spec.js
@@ -1,30 +1,16 @@
-let seqDsl = require('../../../src/parser/index');
-
-function getMessageContext(code) {
-  let rootContext = seqDsl.RootContext(code);
-  expect(seqDsl.RootContext).not.toBeNull()
-  let message = rootContext.block().stat()[0].message();
-  return message;
-}
+import {Fixture} from "./fixture/Fixture";
 
 describe('message - complete', () => {
   test('A.m', () => {
-    let message = getMessageContext('200=A.m');
+    let message = Fixture.firstStatement('200=A.m').message();
     expect(message.SignatureText()).toBe('m');
     expect(message.messageBody().assignment().assignee().getText()).toBe('200');
   })
 })
 
-describe('message - Owner', () => {
-  test('A.m', () => {
-    let message = getMessageContext('A.m');
-    expect(message.Owner()).toBe('A')
-  })
-})
-
 describe('message - incomplete', () => {
   test('A.', () => {
-    let message = getMessageContext('A.')
+    let message = Fixture.firstStatement('A.').message()
     expect(message.messageBody().to().getText()).toBe('A')
   })
 
@@ -32,41 +18,33 @@ describe('message - incomplete', () => {
   // The editor should close the () in most cases. We do not add alternative rules to allow this
   // to be parsed as one statement, because it causes other issues.
   test('A.m(', () => {
-    let message = getMessageContext('A.m(');
+    let message = Fixture.firstStatement('A.m(').message();
     let signatureElement = message.messageBody().func();
     expect(signatureElement).toBeNull()
   })
 })
 
 test('seqDsl should parse a simple method with a method call as parameter', () => {
-    let rootContext = seqDsl.RootContext('B.method(getCount(1))');
-    expect(seqDsl.RootContext).not.toBeNull()
-    let signatureText = rootContext.block().stat()[0].message().SignatureText();
-    expect(signatureText).toBe('method(getCount(1))')
+  let signatureText = Fixture.firstStatement('B.method(getCount(1))').message().SignatureText();
+  expect(signatureText).toBe('method(getCount(1))')
 })
 
 test('seqDsl should parse a simple method with quoted method name', () => {
-    let rootContext = seqDsl.RootContext('B."method. {a,b} 1"(1,2)');
-    expect(seqDsl.RootContext).not.toBeNull()
-    let signatureElement = rootContext.block().stat()[0].message().SignatureText();
-    expect(signatureElement).toBe('"method. {a,b} 1"(1,2)')
+  let signatureElement = Fixture.firstStatement('B."method. {a,b} 1"(1,2)').message().SignatureText();
+  expect(signatureElement).toBe('"method. {a,b} 1"(1,2)')
 })
 
 test('Simple method: A->B.method()', () => {
-    let rootContext = seqDsl.RootContext('A->B.method()');
-    expect(seqDsl.RootContext).not.toBeNull()
-  const message = rootContext.block().stat()[0].message();
+  const message = Fixture.firstStatement('A->B.method()').message();
   let messageBody = message.messageBody();
-    expect(messageBody.from().getText()).toBe('A');
-    expect(message.SignatureText()).toBe('method()');
+  expect(messageBody.from().getText()).toBe('A');
+  expect(message.SignatureText()).toBe('method()');
 })
 
 test('Simple method: "A".method()', () => {
-    let rootContext = seqDsl.RootContext('"A".method()');
-    expect(seqDsl.RootContext).not.toBeNull()
-  const message = rootContext.block().stat()[0].message();
+  const message = Fixture.firstStatement('"A".method()').message();
   let messageBody = message.messageBody();
-    expect(messageBody.to().getTextWithoutQuotes()).toBe('A');
-    expect(message.SignatureText()).toBe('method()');
+  expect(messageBody.to().getTextWithoutQuotes()).toBe('A');
+  expect(message.SignatureText()).toBe('method()');
 })
 
