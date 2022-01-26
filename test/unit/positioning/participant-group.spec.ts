04b9925321af3d1a2fc931c4380d2d3a707f3d69
MrCoder <eagle.xiao@gmail.com>
2022-01-26 20:11:46 +1100

    Refactoring.

--- test/unit/positioning/participant-group.spec.ts
+++ test/unit/positioning/participant-group.spec.ts
@@ -1,24 +1,31 @@
 import {antlr4, ParticipantListener, seqDsl, SingleOrGroup} from "@/positioning/ParticipantListener";
 
+function Participants2(code: string) {
+  const rootContext = seqDsl.RootContext(code);
+  const listener = new ParticipantListener();
+  const walker = antlr4.tree.ParseTreeWalker.DEFAULT
+  walker.walk(listener, rootContext)
+  return listener.result();
+}
+
 describe('participant group', () => {
   it.each([
     ['A', 'A'],
     ['@EC2', 'Missing `Participant` name'],
   ])('Code `%s` produces one single participant with name `%s`', (code, name) => {
-    const rootContext = seqDsl.RootContext(code);
-    const listener = new ParticipantListener();
-    const walker = antlr4.tree.ParseTreeWalker.DEFAULT
-    walker.walk(listener, rootContext)
-    expect(listener.result()).toEqual([{ type: SingleOrGroup.SINGLE, name: name }])
+    const participants2 = Participants2(code);
+    expect(participants2).toEqual([{ type: SingleOrGroup.SINGLE, name: name }])
   })
 
   // it should print out a tree: root { A { B C } }
   it('prints participants', () => {
-    const code = 'A group { B C }'
-    const rootContext = seqDsl.RootContext(code);
-    const listener = new ParticipantListener();
-    const walker = antlr4.tree.ParseTreeWalker.DEFAULT
-    walker.walk(listener, rootContext)
-    expect(listener.result()).toEqual([{"type":0,"name":"A"},{"type":1,"children":[{"type":0,"name":"B"},{"type":0,"name":"C"}]}])
+    const code = 'A group { B C }';
+    const expected = [{"type":0,"name":"A"},
+      {
+        "type":1,"children":[
+          {"type":0,"name":"B"},{"type":0,"name":"C"}
+        ]
+      }];
+    expect(Participants2(code)).toEqual(expected)
   })
 })
