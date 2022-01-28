1328d4596459cfae9234c58efa42d5e75deddfcc
MrCoder <eagle.xiao@gmail.com>
2022-01-28 20:34:40 +1100

    Copied methods from PosCal3 to PosCal2.

--- src/positioning/PosCal2.ts
+++ src/positioning/PosCal2.ts
@@ -1,8 +1,11 @@
 // a, b, c, MIN_GAP=100, MARGIN
 // [{participant: a, gap:100, width: 250 }, {p: b, g:100, w: 120 }, {p: c, g: 150, w: 200}]
 // delta {a: {g: 110, w: 120} =>
-import {ICoordinate2, WidthFunc} from "@/positioning/Coordinate";
-import {PosCal3} from "@/positioning/PosCal3";
+import {ICoordinate2, ICoordinates2, TextType, WidthFunc} from "@/positioning/Coordinate";
+import {MessageContextListener} from "@/positioning/MessageContextListener";
+import {OrderedParticipants} from "@/positioning/OrderedParticipants";
+import {antlr4, IParticipantModel} from "@/positioning/ParticipantListener";
+import {OwnableMessage} from "@/positioning/OwnableMessage";
 
 export class PosCal2 {
   private _participants: Array<ICoordinate2>;
@@ -10,7 +13,7 @@ export class PosCal2 {
   MARGIN = 20;
 
   constructor(ctx: any, widthProvider: WidthFunc) {
-    this._participants = PosCal3.getMessageWidthAndParticipantWidth(ctx, widthProvider);
+    this._participants = PosCal2.getMessageWidthAndParticipantWidth(ctx, widthProvider);
   }
 
   getPosition(participantName: string|undefined): number {
@@ -31,9 +34,35 @@ export class PosCal2 {
 
   half(participant: ICoordinate2): number {
     if (participant) {
-      // get previous participant
       return (participant.participantWidth / 2) + (this.MARGIN / 2);
     }
     return 0;
   }
+
+  private static getAllMessages(ctx: any) {
+    const walker = antlr4.tree.ParseTreeWalker.DEFAULT
+
+    const listener = new MessageContextListener();
+    walker.walk(listener, ctx);
+    return listener.result();
+  }
+
+  private static MIN_MESSAGE_WIDTH = 100;
+  static getMessageWidthAndParticipantWidth(ctx: any, widthProvider: WidthFunc): ICoordinates2 {
+    let ownedMessagesList = PosCal2.getAllMessages(ctx);
+    const participantModels = OrderedParticipants(ctx);
+
+    return participantModels.map((p: IParticipantModel) => {
+      const participant = p.name;
+      const leftNeighbour = p.left;
+      const myMessage = ownedMessagesList.find(p1 => p1.owner === p.name);
+      const contributingMessages = myMessage?.ownableMessages?.filter(o => o.from === leftNeighbour) || [];
+      const messageWidth = contributingMessages.reduce((acc, m: OwnableMessage) => {
+        return Math.max(acc, widthProvider(m.signature || '', TextType.MessageContent) + 10);
+      }, this.MIN_MESSAGE_WIDTH);
+      const participantWidth = widthProvider(participant || '', TextType.ParticipantName);
+      return {participant, messageWidth, participantWidth} as ICoordinate2;
+    });
+  }
+
 }
