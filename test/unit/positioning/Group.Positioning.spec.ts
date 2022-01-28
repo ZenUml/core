e416fe72e232886691c8f10f3ffdfff66d605f7c
MrCoder <eagle.xiao@gmail.com>
2022-01-28 13:29:06 +1100

    Refactoring.

--- test/unit/positioning/Group.Positioning.spec.ts
+++ test/unit/positioning/Group.Positioning.spec.ts
@@ -23,38 +23,31 @@ function getGroupCoordinates(participants: IParticipantModel[], coordinates2: IC
   let result = [] as any;
   const posCal2 = new PosCal2(coordinates2);
 
-  function _processSingleParticipantWithRelativeLeft(participant: IParticipantModel, relativeLeft: number) {
-    let left = posCal2.getPosition(participant.name || '') || 0
-    result.push({
-      key: participant.key,
-      name: participant.name,
-      left: left - relativeLeft
-    })
+  function _getLeft(participant: IParticipantModel) {
+    let positionProvider = participant.type === SingleOrGroup.GROUP ? participant.children[0] : participant;
+    return posCal2.getPosition(positionProvider.name || '') || 0;
   }
 
-  function _processGroup(participant: IParticipantModel) {
-    let firstChild = participant.children[0];
-    let left = posCal2.getPosition(firstChild.name || '') || 0
-    result.push({
-      key: participant.key,
-      name: participant.name,
-      left: left
-    })
-    for (let j = 0; j < participant.children.length; j++) {
-      _processSingleParticipantWithRelativeLeft(participant.children[j], left);
+  function _processItem(participant: IParticipantModel, relativeLeft: number = 0) {
+    let left = _getLeft(participant);
+    result.push({key: participant.key, name: participant.name, left: left - relativeLeft})
+    for (const item of participant.children) {
+      _processItem(item, left);
     }
   }
 
-  for (let i = 0; i < participants.length; i++) {
-    const participant = participants[i];
-    if (participant.type === SingleOrGroup.SINGLE) {
-      _processSingleParticipantWithRelativeLeft(participant, 0);
-    }
-    if (participant.type === SingleOrGroup.GROUP) {
-      _processGroup(participant);
-    }
+  for (const participant of participants) {
+    _processItem(participant);
   }
   return result;
+}
+
+function CoordinateCalc(rootContext: any) {
+  let participants = Participants2(rootContext);
+  const posCal3 = new PosCal3();
+  let coordinates2 = posCal3.getCoordinates2(rootContext, stubWidthProvider);
+
+  return getGroupCoordinates(participants, coordinates2);
 }
 
 describe('Group Positioning', () => {
@@ -65,18 +58,14 @@ describe('Group Positioning', () => {
   })
 
   it('position groups', () => {
-    let rootContext = seqDsl.RootContext('A100 group {B100}');
-    let participants = Participants2(rootContext);
-    const posCal3 = new PosCal3();
-    let coordinates2 = posCal3.getCoordinates2(rootContext, stubWidthProvider);
-
-    let groupCoordinates = getGroupCoordinates(participants, coordinates2);
+    let rootContext = seqDsl.RootContext('A100 group {B100} C100');
+    let groupCoordinates = CoordinateCalc(rootContext);
     const expected = [
       { key: '0-3', name: 'A100', left: 100 },
       { key: '5-16', name: undefined, left: 220 },
-      { key: '12-15', name: 'B100', left: 0 }
+      { key: '12-15', name: 'B100', left: 0 },
+      { key: '18-21', name: 'C100', left: 340 }
     ]
-    console.log('groupCoordinates', groupCoordinates);
     expect(groupCoordinates).toEqual(expected);
   })
 
