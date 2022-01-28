de1d4cb114a4c5ff686ea80fbd5ca90fdffb52bb
MrCoder <eagle.xiao@gmail.com>
2022-01-28 19:00:45 +1100

    Refactoring.

--- src/positioning/PosCal3.ts
+++ src/positioning/PosCal3.ts
@@ -1,7 +1,7 @@
 import {OrderedParticipants} from "@/positioning/OrderedParticipants";
 import {MessageContextListener} from "./MessageContextListener";
 import {ICoordinate2, ICoordinates2, TextType, WidthFunc} from "./Coordinate";
-import {IOwnedMessages} from "@/positioning/OwnableMessage";
+import {IOwnedMessages, OwnableMessage} from "@/positioning/OwnableMessage";
 import {IParticipantModel} from "@/positioning/ParticipantListener";
 
 const antlr4 = require('antlr4/index');
@@ -40,22 +40,19 @@ export class PosCal3 {
   static getMessageWidthAndParticipantWidth(ctx: any, widthProvider: WidthFunc): ICoordinates2 {
     let ownedMessagesList = PosCal3.getAllMessages(ctx);
     const participantModels = OrderedParticipants(ctx);
-    ownedMessagesList = participantModels.map((participant: IParticipantModel) => {
-      return PosCal3.getOwnedMessages(ownedMessagesList, participant.name || '');
-    });
 
     return participantModels.map((p: IParticipantModel) => {
       const participant = p.name;
       const leftNeighbour = p.left;
       const myMessage = ownedMessagesList.find(p1 => p1.owner === p.name);
-      const messageWidth = PosCal3.getMessageWidth(widthProvider, myMessage, leftNeighbour);
+      const messageWidth = PosCal3.getMessageWidth(widthProvider, myMessage?.ownableMessages || [], leftNeighbour);
       const participantWidth = widthProvider(participant || '', TextType.ParticipantName);
       return {participant, messageWidth, participantWidth} as ICoordinate2;
     });
   }
 
-  private static getMessageWidth(widthProvider: WidthFunc, p: IOwnedMessages | undefined, leftNeighbour: string | undefined) {
-    const contributingMessages = p?.ownableMessages.filter(o => o.from === leftNeighbour);
+  private static getMessageWidth(widthProvider: WidthFunc, ownableMessages: Array<OwnableMessage>, leftNeighbour: string | undefined): number {
+    const contributingMessages = ownableMessages?.filter(o => o.from === leftNeighbour);
     const messageWidth = contributingMessages?.map((m: any) => {
       // 10px for the arrow head
       return widthProvider(m.signature, TextType.MessageContent) + 10;
@@ -63,8 +60,4 @@ export class PosCal3 {
     // return the max width for messages
     return Math.max(...messageWidth || [], this.MIN_MESSAGE_WIDTH);
   }
-
-  // An owned message always has 'from';
-  // root messages has 'from' as _STARTER_;
-  // 'from' can be itself; special case: @Starter(A) A.method(), from === A
 }
