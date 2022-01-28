e4a22544da095dfdfb9949c39acc53e829f521e6
MrCoder <eagle.xiao@gmail.com>
2022-01-28 19:09:02 +1100

    Inlined getMessageWidth.

--- src/positioning/PosCal3.ts
+++ src/positioning/PosCal3.ts
@@ -17,6 +17,11 @@ export class PosCal3 {
     });
   }
 
+  private static getOwnedMessages(ownedMessagesList: Array<IOwnedMessages>, participant: string) {
+    const ownedMessages = ownedMessagesList.find(o => o.owner === participant);
+    return ownedMessages || {owner: participant, ownableMessages: []};
+  }
+
   private static getAllParticipants(ctx: any) {
     return OrderedParticipants(ctx).map((participant: any) => {
       return participant.name;
@@ -31,11 +36,6 @@ export class PosCal3 {
     return listener.result();
   }
 
-  private static getOwnedMessages(ownedMessagesList: Array<IOwnedMessages>, participant: string) {
-    const ownedMessages = ownedMessagesList.find(o => o.owner === participant);
-    return ownedMessages || {owner: participant, ownableMessages: []};
-  }
-
   private static MIN_MESSAGE_WIDTH = 100;
   static getMessageWidthAndParticipantWidth(ctx: any, widthProvider: WidthFunc): ICoordinates2 {
     let ownedMessagesList = PosCal3.getAllMessages(ctx);
@@ -45,19 +45,12 @@ export class PosCal3 {
       const participant = p.name;
       const leftNeighbour = p.left;
       const myMessage = ownedMessagesList.find(p1 => p1.owner === p.name);
-      const messageWidth = PosCal3.getMessageWidth(widthProvider, myMessage?.ownableMessages || [], leftNeighbour);
+      const contributingMessages = myMessage?.ownableMessages?.filter(o => o.from === leftNeighbour) || [];
+      const messageWidth = contributingMessages.reduce((acc, m: OwnableMessage) => {
+        return Math.max(acc, widthProvider(m.signature || '', TextType.MessageContent) + 10);
+      }, this.MIN_MESSAGE_WIDTH);
       const participantWidth = widthProvider(participant || '', TextType.ParticipantName);
       return {participant, messageWidth, participantWidth} as ICoordinate2;
     });
-  }
-
-  private static getMessageWidth(widthProvider: WidthFunc, ownableMessages: Array<OwnableMessage>, leftNeighbour: string | undefined): number {
-    const contributingMessages = ownableMessages?.filter(o => o.from === leftNeighbour);
-    const messageWidth = contributingMessages?.map((m: any) => {
-      // 10px for the arrow head
-      return widthProvider(m.signature, TextType.MessageContent) + 10;
-    });
-    // return the max width for messages
-    return Math.max(...messageWidth || [], this.MIN_MESSAGE_WIDTH);
   }
 }
