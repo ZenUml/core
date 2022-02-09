cd406f784318684813223228e9e64a896e1d1988
MrCoder <eagle.xiao@gmail.com>
2022-02-09 17:23:14 +1100

    Renamed the methods.

--- test/unit/positioning/MatrixBasedAlgorithm.spec.ts
+++ test/unit/positioning/MatrixBasedAlgorithm.spec.ts
@@ -1,40 +1,44 @@
 let m = Array<Array<number>>();
-let p = (i: number, j: number) => {
+let min_distance = (i: number, j: number) => {
   return m[i][j];
 }
-let r = (i: number, j: number): number => {
+let final_distance = (i: number, j: number): number => {
   if (j === i) {
     return 0;
   } else if(j - i === 1) {
     let temp = [];
     for(let k = 0; k <= i; k++) {
-      temp.push(p(k, j) - r(k, i));
+      temp.push(min_distance(k, j) - final_distance(k, i));
     }
     return Math.max(...temp)
   } else {
     let sum = 0;
     for(let l = i; l < j; l++) {
-      sum += r(l, l + 1);
+      sum += final_distance(l, l + 1);
     }
     return sum;
   }
+}
+
+let final_pos = (i: number): number => {
+  return final_distance(0, i);
 }
 
 describe('go', () => {
   it('go', () => {
     m = [
       [0, 100, 100, 800],
-      [0, 0, 100, 0],
-      [0, 0, 0, 100],
-      [0, 0, 0, 0],
+      [0, 0,   100, 0],
+      [0, 0,   0,   100],
+      [0, 0,   0,   0],
     ]
-    expect(r(0, 0)).toBe(0)
-    expect(r(0, 1)).toBe(100)
-    expect(r(0, 2)).toBe(200)
-    expect(r(0, 3)).toBe(800)
-    expect(r(1, 2)).toBe(100)
-    expect(r(1, 3)).toBe(700)
-    expect(r(2, 3)).toBe(600)
+    expect(final_distance(0, 0)).toBe(0)
+    expect(final_distance(0, 1)).toBe(100)
+    expect(final_distance(0, 2)).toBe(200)
+    expect(final_distance(0, 3)).toBe(800)
+    expect(final_distance(1, 2)).toBe(100)
+    expect(final_distance(1, 3)).toBe(700)
+    expect(final_distance(2, 3)).toBe(600)
   })
 
   it('go2', () => {
@@ -44,13 +48,13 @@ describe('go', () => {
       [0, 0, 0, 0],
       [0, 0, 0, 0],
     ]
-    expect(r(0, 0)).toBe(0)
-    expect(r(0, 1)).toBe(100)
-    expect(r(0, 2)).toBe(400)
-    expect(r(0, 3)).toBe(900)
-    expect(r(1, 2)).toBe(300)
-    expect(r(1, 3)).toBe(800)
-    expect(r(2, 3)).toBe(500)
+    expect(final_distance(0, 0)).toBe(0)
+    expect(final_distance(0, 1)).toBe(100)
+    expect(final_distance(0, 2)).toBe(400)
+    expect(final_distance(0, 3)).toBe(900)
+    expect(final_distance(1, 2)).toBe(300)
+    expect(final_distance(1, 3)).toBe(800)
+    expect(final_distance(2, 3)).toBe(500)
   })
 
   it('go3', () => {
@@ -61,16 +65,16 @@ describe('go', () => {
       [0, 0, 0, 0, 200],
       [0, 0, 0, 0, 0],
     ]
-    expect(r(0, 0)).toBe(0)
-    expect(r(0, 1)).toBe(300)
-    expect(r(0, 2)).toBe(400)
-    expect(r(0, 3)).toBe(700)
-    expect(r(0, 4)).toBe(1300)
-    expect(r(1, 2)).toBe(100)
-    expect(r(1, 3)).toBe(400)
-    expect(r(1, 4)).toBe(1000)
-    expect(r(2, 3)).toBe(300)
-    expect(r(2, 4)).toBe(900)
-    expect(r(3, 4)).toBe(600)
+    expect(final_distance(0, 0)).toBe(0)
+    expect(final_distance(0, 1)).toBe(300)
+    expect(final_distance(0, 2)).toBe(400)
+    expect(final_distance(0, 3)).toBe(700)
+    expect(final_distance(0, 4)).toBe(1300)
+    expect(final_distance(1, 2)).toBe(100)
+    expect(final_distance(1, 3)).toBe(400)
+    expect(final_distance(1, 4)).toBe(1000)
+    expect(final_distance(2, 3)).toBe(300)
+    expect(final_distance(2, 4)).toBe(900)
+    expect(final_distance(3, 4)).toBe(600)
   })
 })
