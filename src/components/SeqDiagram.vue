772690a69cfb7f70c2920a206e2cf446e1b8799b
MrCoder <eagle.xiao@gmail.com>
2022-02-14 16:45:41 +1100

    hotfix: added extra margin left.
    if (x) { if(y) { if(z) {} } }

--- src/components/SeqDiagram.vue
+++ src/components/SeqDiagram.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- py-8 to give space for icons -->
-  <div class="sequence-diagram py-8" ref="diagram" >
+  <div class="sequence-diagram py-8 ml-8" ref="diagram" >
     <life-line-layer :context="rootContext.head()"/>
     <message-layer :context="rootContext.block()" :key="generation"/>
   </div>
