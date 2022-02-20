6f8dc20e1f32d250851b4c65c3bb4a5af65c5495
MrCoder <eagle.xiao@gmail.com>
2022-02-20 20:20:31 +1100

    Make sure `isCurrent` does not throw errors up.

--- src/parser/IsCurrent.js
+++ src/parser/IsCurrent.js
@@ -1,22 +1,26 @@
 const sequenceParser = require('../generated-parser/sequenceParser')
-
 const seqParser = sequenceParser.sequenceParser;
-const CreationContext = seqParser.CreationContext;
-CreationContext.prototype.isCurrent = function (cursor) {
-  if (cursor === null || cursor === undefined) return false;
-  const start = this.start.start;
-  const stop = this.creationBody().stop.stop + 1;
 
-  return cursor >= start && cursor <= stop;
+const CreationContext = seqParser.CreationContext;
+CreationContext.prototype.Body = CreationContext.prototype.creationBody;
+CreationContext.prototype.isCurrent = function (cursor) {
+  return isCurrent.bind(this)(cursor);
 }
 
-
 const MessageContext = seqParser.MessageContext
-
+MessageContext.prototype.Body = MessageContext.prototype.messageBody;
 MessageContext.prototype.isCurrent = function (cursor) {
-  if (cursor === null || cursor === undefined) return false;
-  const start = this.start.start;
-  const stop = this.messageBody().stop.stop + 1;
+  return isCurrent.bind(this)(cursor);
+}
 
-  return cursor >= start && cursor <= stop;
+function isCurrent(cursor) {
+  try {
+    if (cursor === null || cursor === undefined) return false;
+    const start = this.start.start;
+    const stop = this.Body().stop.stop + 1;
+
+    return cursor >= start && cursor <= stop;
+  } catch (e) {
+    return false;
+  }
 }
