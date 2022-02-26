f360d29741361fbc2557c85a4f7a1917b3e67114
MrCoder <eagle.xiao@gmail.com>
2022-02-26 14:37:26 +1100

    Added zoom in/out buttons.

--- src/components/DiagramFrame.vue
+++ src/components/DiagramFrame.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- pb-8 is to offset pt-8 in SeqDiagram component -->
-  <div class="frame relative pb-8 ml-1">
+  <div class="frame relative ml-1 origin-top-left" :style="{transform: `scale(${scale})`}">
     <div class="header flex">
       <div class="left">
         <slot></slot>
@@ -15,11 +15,18 @@
       </div>
     </div>
     <seq-diagram/>
-    <button class="absolute bottom-1 left-1" @click="showTipsDialog()">
-      <svg class="filter grayscale" style="width: 1em; height: 1em;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M514 912c-219.9 0-398.8-178.9-398.8-398.9 0-219.9 178.9-398.8 398.8-398.8s398.9 178.9 398.9 398.8c-0.1 220-179 398.9-398.9 398.9z m0-701.5c-166.9 0-302.7 135.8-302.7 302.7S347.1 815.9 514 815.9s302.7-135.8 302.7-302.7S680.9 210.5 514 210.5z" fill="#BDD2EF" /><path d="M431.1 502.4c-0.1 0.3 0.3 0.4 0.4 0.2 6.9-11.7 56.5-89.1 23.4 167.3-17.4 134.7 122.9 153.6 142.3-7.9 0.1-1-1.3-1.4-1.7-0.4-11.9 37.2-49.6 104.9-4.7-155.2 18.6-107.2-127.6-146-159.7-4z" fill="#2867CE" /><path d="M541.3 328m-68 0a68 68 0 1 0 136 0 68 68 0 1 0-136 0Z" fill="#2867CE" /></svg>
-    </button>
-    <width-provider/>
-    <a target="_blank" href="https://zenuml.com" class="absolute bottom-1 right-1 text-xs opacity-0 hover:opacity-100 duration-300 ">ZenUML.com</a>
+    <div class="footer mt-10 flex justify-between">
+      <button class="bottom-1 left-1" @click="showTipsDialog()">
+        <svg class="filter grayscale" style="width: 1em; height: 1em;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M514 912c-219.9 0-398.8-178.9-398.8-398.9 0-219.9 178.9-398.8 398.8-398.8s398.9 178.9 398.9 398.8c-0.1 220-179 398.9-398.9 398.9z m0-701.5c-166.9 0-302.7 135.8-302.7 302.7S347.1 815.9 514 815.9s302.7-135.8 302.7-302.7S680.9 210.5 514 210.5z" fill="#BDD2EF" /><path d="M431.1 502.4c-0.1 0.3 0.3 0.4 0.4 0.2 6.9-11.7 56.5-89.1 23.4 167.3-17.4 134.7 122.9 153.6 142.3-7.9 0.1-1-1.3-1.4-1.7-0.4-11.9 37.2-49.6 104.9-4.7-155.2 18.6-107.2-127.6-146-159.7-4z" fill="#2867CE" /><path d="M541.3 328m-68 0a68 68 0 1 0 136 0 68 68 0 1 0-136 0Z" fill="#2867CE" /></svg>
+      </button>
+      <div class="zoom-controls flex justify-between w-28 bg-blue-100" :style="{transform: `scale(${1/scale})`}">
+        <button class="zoom-in px-1" @click="zoomIn()">+</button>
+        <label>{{Number(scale * 100).toFixed(0)}} %</label>
+        <button class="zoom-out  px-1" @click="zoomOut()">-</button>
+      </div>
+      <width-provider/>
+      <a target="_blank" href="https://zenuml.com" class="absolute bottom-1 right-1 text-xs opacity-0 hover:opacity-100 duration-300 ">ZenUML.com</a>
+    </div>
   </div>
 </template>
 
@@ -33,6 +40,11 @@ import WidthProvider from "@/components/positioning/WidthProvider";
 
 export default {
   name: "DiagramFrame",
+  data() {
+    return {
+      scale: 1,
+    }
+  },
   computed: {
     ...mapState(['showTips']),
     ...mapGetters(['rootContext']),
@@ -54,7 +66,13 @@ export default {
       } catch (e) {
         console.error(e)
       }
-    }
+    },
+    zoomIn() {
+      this.scale += 0.1;
+    },
+    zoomOut() {
+      this.scale -= 0.1;
+    },
   },
   components: {
     WidthProvider,
