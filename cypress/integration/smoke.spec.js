d8de7f1464084f94691ec0dd065484ff97b7b2a6
MrCoder <eagle.xiao@gmail.com>
2022-03-19 11:36:46 +1100

    Try to fix an error running cypress test.

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -2,7 +2,11 @@
 import 'cypress-plugin-snapshots/commands';
 describe('Smoke test', function () {
   it('should load the home page', function () {
-    cy.visit('http://localhost:8080/smoke.html')
+    cy.visit('http://localhost:8080/smoke.html', {
+      headers: {
+        "Accept-Encoding": "gzip, deflate"
+      }
+    })
       .then(() => {
         cy.document().toMatchImageSnapshot({
           imageConfig: {"threshold": 0.001}, capture: "fullPage"
