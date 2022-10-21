92ad103c1c1a18e7dbf5ce7ce012e85161b74e4c
MrCoder <eagle.xiao@gmail.com>
2022-10-22 07:53:30 +1100

    useEsBuild for Js, Ts and Css.
    It saves around 20s on macbook air M1.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,42 @@
+const { ESBuildMinifyPlugin } = require('esbuild-loader')
 const {execSync} = require("child_process");
 process.env.VUE_APP_GIT_HASH = execSync('git rev-parse --short HEAD').toString().trim()
 process.env.VUE_APP_GIT_BRANCH = execSync('git branch --show-current').toString().trim();
+// saves ~6 seconds on macbook air m1
+function useEsBuildForCss(config) {
+  // 删除底层 terser, 换用 esbuild-minimize-plugin
+  config.optimization.minimizers.delete('terser');
+
+  // 使用 esbuild 优化 css 压缩
+  config.optimization
+      .minimizer('esbuild')
+      .use(ESBuildMinifyPlugin, [{minify: true, css: true}]);
+}
+
+// saves ~6 seconds on macbook air m1
+function useEsBuildForTs(config) {
+  const ruleTs = config.module.rule('ts');
+  // clear babel-loader
+  ruleTs.uses.clear()
+
+  // add esbuild-loader
+  ruleTs.use('esbuild-loader').loader('esbuild-loader')
+      .options({
+        loader: 'ts', // 如果使用了 ts, 或者 vue 的 class 装饰器，则需要加上这个 option 配置， 否则会报错： ERROR: Unexpected "@"
+        target: 'es2015',
+        tsconfigRaw: require('./tsconfig.json')
+      });
+}
+
+// saves ~6 seconds on macbook air m1
+function useEsBuildForJs(config) {
+  const rule = config.module.rule('js');
+  // clear babel-loader
+  rule.uses.clear()
+
+  // add esbuild-loader
+  rule.use('esbuild-loader').loader('esbuild-loader');
+}
 
 module.exports = {
   pages: {
@@ -49,6 +85,9 @@ module.exports = {
     // A workaround that allows npm link or yarn link
     // https://cli.vuejs.org/guide/troubleshooting.html#symbolic-links-in-node-modules
     config.resolve.symlinks(false)
+    useEsBuildForJs(config);
+    useEsBuildForTs(config);
+    useEsBuildForCss(config);
 
     config.plugin('define').tap((definitions) => {
       definitions[0]['VERSION'] = JSON.stringify(require('./package.json').version);
