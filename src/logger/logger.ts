c9faf782ae36a3ca61a219542a10c09e446a0ebe
MrCoder <eagle.xiao@gmail.com>
2022-11-19 19:24:26 +1100

    Added pino-pretty-browser as a dependency.

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,37 +1,12 @@
 import pino from 'pino';
-import {prettyFactory} from '../pino-pretty-browser/index'
-const pretty = prettyFactory({ sync: true, colorize: true, singleLine: true });
+import {write} from 'pino-pretty-browser'
 
 const logger = pino({
   level: 'debug',
   browser: {
     asObject: true,
     serialize: true,
-    write: (inputData) => {
-      const prettifiedOutput = pretty(inputData);
-      // switch on inputData.level and map to console.log, console.warn, console.error, etc.
-      // @ts-ignore
-      switch (inputData.level) {
-        case 10:
-          console.trace(prettifiedOutput);
-          break;
-        case 20:
-          console.debug(prettifiedOutput);
-          break;
-        case 30:
-          console.info(prettifiedOutput);
-          break;
-        case 40:
-          console.warn(prettifiedOutput);
-          break;
-        case 50:
-        case 60:
-          console.error(prettifiedOutput);
-          break;
-        default:
-          console.log(prettifiedOutput);
-      }
-    },
+    write: write(),
   }
 });
 
