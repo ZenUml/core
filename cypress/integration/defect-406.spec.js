d2af1de1d285c47511d93db1acf60cbf70fe69b5
MrCoder <eagle.xiao@gmail.com>
2022-11-24 22:08:45 +1100

    Fixed cy test.

--- cypress/integration/defect-406.spec.js
+++ cypress/integration/defect-406.spec.js
@@ -2,18 +2,10 @@
 import 'cypress-plugin-snapshots/commands';
 describe('Defect 406', function () {
   it('Fragments under Creation', function () {
-    cy.visit('http://localhost:8080/cy/defect-406-alt-under-creation.html', {
-      headers: {
-        "Accept-Encoding": "gzip, deflate"
-      },
-      retryOnStatusCodeFailure: true,
-      retryOnNetworkFailure: true,
-      timeout: 30000
-    })
-      .then(() => {
-        cy.document().toMatchImageSnapshot({
-          imageConfig: {"threshold": 0.005}, capture: "viewport"
-        });
-      })
+    cy.visit('http://localhost:8080/cy/defect-406-alt-under-creation.html');
+    cy.wait(1000);
+    cy.document().toMatchImageSnapshot({
+      imageConfig: {"threshold": 0.005}, capture: "viewport"
+    });
   });
 });
