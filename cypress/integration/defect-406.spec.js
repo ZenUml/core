e5fc4e057ed6598fa6daab285b20a4da043cb853
MrCoder <eagle.xiao@gmail.com>
2022-12-01 15:14:52 +1100

    Updated cy test images.

--- cypress/integration/defect-406.spec.js
+++ cypress/integration/defect-406.spec.js
@@ -3,7 +3,7 @@ import 'cypress-plugin-snapshots/commands';
 describe('Defect 406', function () {
   it('Fragments under Creation', function () {
     cy.visit('http://localhost:8080/cy/defect-406-alt-under-creation.html');
-    cy.wait(1000);
+    cy.wait(1500);
     cy.document().toMatchImageSnapshot({
       imageConfig: {"threshold": 0.005}, capture: "viewport"
     });
