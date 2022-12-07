f86109385a1c061037c66a35d7023cce07504ec1
MrCoder <eagle.xiao@gmail.com>
2022-12-07 19:23:32 +1100

    Base theme works fine with fallbacks.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ A
 A.method`;
 // @ts-ignore
 const zenUml = new ZenUml(elm);
-zenUml.render(code, 'default').then(r => {
+zenUml.render(code, '').then(r => {
   logger.debug('render resolved', r);
 });
 // @ts-ignore
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,34 +6,34 @@ module.exports = {
     extend: {
       textColor: {
         skin: {
-          title: 'var(--color-text-title)',
+          title: 'var(--color-text-title, var(--color-text-message, var(--color-text-base, #000)))',
           participant: 'var(--color-text-participant)',
-          message: 'var(--color-text-message)',
-          "message-arrow": 'var(--color-message-arrow)', // message arrow head
-          "message-hover": 'var(--color-text-message-hover)',
-          comment: 'var(--color-text-comment)',
-          fragment: 'var(--color-text-fragment)',
+          message: 'var(--color-text-message, var(--color-text-base, #000))',
+          "message-arrow": 'var(--color-message-arrow, var(--color-border-frame, var(--color-border-base, #000)))', // message arrow head
+          "message-hover": 'var(--color-text-message-hover, var(--color-bg-base, #fff))',
+          comment: 'var(--color-text-comment, var(--color-text-base, #000))',
+          fragment: 'var(--color-text-fragment, var(--color-text-message, #000))',
           base: 'var(--color-text-base)',
           header: 'var(--color-text-header)',
           secondary: 'var(--color-text-secondary)',
-          control: 'var(--color-text-control)',
+          control: 'var(--color-text-control, var(--color-text-base, #000))',
           muted: 'var(--color-text-muted)',
           hover: 'var(--color-text-hover)',
-          link: `var(--color-text-link)`,
+          link: `var(--color-text-link, var(--color-text-base, #000))`,
           fill: `var(--color-text-fill)`,
         },
       },
       backgroundColor: {
         skin: {
-          canvas: 'var(--color-bg-canvas)',
-          frame: 'var(--color-bg-frame)',
-          title: 'var(--color-bg-title)',
-          participant: 'var(--color-bg-participant)',
-          lifeline: 'var(--color-border-participant)',
+          canvas: 'var(--color-bg-canvas, var(--color-bg-base, #fff))',
+          frame: 'var(--color-bg-frame, var(--color-bg-canvas, var(--color-bg-base, #fff)))',
+          title: 'var(--color-bg-title, var(--color-bg-frame, var(--color-bg-canvas, var(--color-bg-base, #fff))))',
+          participant: 'var(--color-bg-participant, var(--color-bg-frame, var(--color-bg-canvas, var(--color-bg-base, #fff))))',
+          lifeline: 'var(--color-border-participant, var(--color-border-participant, var(--color-border-frame, var(--color-border-base, #000))))',
           divider: 'var(--color-border-participant)',
-          "message-hover": 'var(--color-bg-message-hover)',
-          "fragment-header": 'var(--color-bg-fragment-header)',
-          occurrence: 'var(--color-bg-occurrence)',
+          "message-hover": 'var(--color-bg-message-hover, var(--color-text-base, #000))',
+          "fragment-header": 'var(--color-bg-fragment-header, var(--color-bg-participant, var(--color-bg-frame, var(--color-bg-canvas, var(--color-bg-base, #fff)))))',
+          occurrence: 'var(--color-bg-occurrence, var(--color-bg-participant, var(--color-bg-frame, var(--color-bg-canvas, var(--color-bg-base, #fff)))))',
           base: 'var(--color-bg-base)',
           secondary: 'var(--color-bg-secondary)',
           hover: 'var(--color-bg-hover)',
@@ -42,11 +42,11 @@ module.exports = {
       },
       borderColor: {
         skin: {
-          frame: 'var(--color-border-frame)',
-          participant: 'var(--color-border-participant)',
-          "message-arrow": 'var(--color-message-arrow)', // message arrow line
-          fragment: 'var(--color-border-fragment)',
-          occurrence: 'var(--color-border-occurrence)',
+          frame: 'var(--color-border-frame, var(--color-border-base, #000))',
+          participant: 'var(--color-border-participant, var(--color-border-frame, var(--color-border-base, #000)))',
+          "message-arrow": 'var(--color-message-arrow, var(--color-border-frame,  var(--color-border-base, #000)))', // message arrow line
+          fragment: 'var(--color-border-fragment, var(--color-border-frame,  var(--color-border-base, #000)))',
+          occurrence: 'var(--color-border-occurrence, var(--color-border-frame,  var(--color-border-base, #000)))',
           base: 'var(--color-border-base)',
           secondary: 'var(--color-border-secondary)',
         },
