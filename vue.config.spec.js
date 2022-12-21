83ee83edf834da5250e09daf08d55bbf285384bf
MrCoder <eagle.xiao@gmail.com>
2022-12-21 14:49:54 +1100

    Fixed vue.config.spec.js.

--- vue.config.spec.js
+++ vue.config.spec.js
@@ -2,53 +2,7 @@ import { describe, expect, it } from 'vitest'
 const config = require('./vue.config')
 describe('vue.config.js', function () {
   it('should get pages', () => {
-    const expectedPages = {
-      "cy/smoke-creation.html": {
-        "entry": "src/main.ts",
-        "filename": "cy/smoke-creation.html",
-        "template": "public/cy/smoke-creation.html"
-      },
-      "cy/smoke-fragment-issue.html": {
-        "entry": "src/main.ts",
-        "filename": "cy/smoke-fragment-issue.html",
-        "template": "public/cy/smoke-fragment-issue.html"
-      },
-      "cy/smoke-fragment.html": {
-        "entry": "src/main.ts",
-        "filename": "cy/smoke-fragment.html",
-        "template": "public/cy/smoke-fragment.html"
-      },
-      "cy/smoke-interaction.html": {
-        "entry": "src/main.ts",
-        "filename": "cy/smoke-interaction.html",
-        "template": "public/cy/smoke-interaction.html"
-      },
-      "cy/smoke-return.html": {
-        "entry": "src/main.ts",
-        "filename": "cy/smoke-return.html",
-        "template": "public/cy/smoke-return.html"
-      },
-      "cy/smoke.html": {
-        "entry": "src/main.ts",
-        "filename": "cy/smoke.html",
-        "template": "public/cy/smoke.html"
-      },
-      "embed-container-demo.html": {
-        "entry": "src/main.ts",
-        "filename": "embed-container-demo.html",
-        "template": "public/embed-container-demo.html"
-      },
-      "embed.html": {
-        "entry": "src/main.ts",
-        "filename": "embed.html",
-        "template": "public/embed.html"
-      },
-      "index.html": {
-        "entry": "src/main.ts",
-        "filename": "index.html",
-        "template": "public/index.html"
-      }
-    };
+    const expectedPages = {"embed-container-demo.html":{"entry":"src/main.ts","template":"public/embed-container-demo.html","filename":"embed-container-demo.html"},"cy/smoke-fragment-issue.html":{"entry":"src/main.ts","template":"public/cy/smoke-fragment-issue.html","filename":"cy/smoke-fragment-issue.html"},"cy/smoke-creation.html":{"entry":"src/main.ts","template":"public/cy/smoke-creation.html","filename":"cy/smoke-creation.html"},"cy/smoke-interaction.html":{"entry":"src/main.ts","template":"public/cy/smoke-interaction.html","filename":"cy/smoke-interaction.html"},"cy/defect-406-alt-under-creation.html":{"entry":"src/main.ts","template":"public/cy/defect-406-alt-under-creation.html","filename":"cy/defect-406-alt-under-creation.html"},"cy/smoke.html":{"entry":"src/main.ts","template":"public/cy/smoke.html","filename":"cy/smoke.html"},"cy/smoke-return.html":{"entry":"src/main.ts","template":"public/cy/smoke-return.html","filename":"cy/smoke-return.html"},"cy/smoke-fragment.html":{"entry":"src/main.ts","template":"public/cy/smoke-fragment.html","filename":"cy/smoke-fragment.html"},"embed.html":{"entry":"src/main.ts","template":"public/embed.html","filename":"embed.html"}};
     expect(config.pages).toEqual(expectedPages)
   })
 });
