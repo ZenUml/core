df877832b86528e30d2fabe89af8866becd93c07
MrCoder <eagle.xiao@gmail.com>
2022-12-21 17:02:37 +1100

    Added 'build' with vite.

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite'
 import { createVuePlugin as vue } from 'vite-plugin-vue2'
 
 export default defineConfig({
+    build: {
+      lib: {
+        entry: 'src/core.ts',
+        name: '@zenuml/core',
+        fileName: 'zenuml-core',
+      }
+    },
     plugins: [vue()],
     test: {
-      environment:"jsdom",
+      environment: 'jsdom',
       deps: {
         inline: [
-          ""
+          ''
         ]
       }
     }
