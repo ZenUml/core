4c4cf7f757af939f611427c8f7745f9b77b77f75
MrCoder <eagle.xiao@gmail.com>
2022-12-27 17:43:10 +1100

    Stabilize smoke test with waiting for an element.

--- cypress/e2e/smoke.spec.js
+++ cypress/e2e/smoke.spec.js
@@ -2,8 +2,8 @@
 import 'cypress-plugin-snapshots/commands';
 describe('Smoke test', function () {
   it('should load the home page', function () {
-    cy.visit('http://127.0.0.1:8080/cy/smoke.html')
-    cy.wait(1500);
+    cy.visit('http://127.0.0.1:8080/cy/smoke.html');
+    cy.get('[data-signature="append(line)"]', {timeout: 5000}).should('be.visible');
     cy.document().toMatchImageSnapshot({
       imageConfig: {"threshold": 0.01}, capture: "viewport"
     });
