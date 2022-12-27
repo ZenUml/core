0b181deff4d33f67e71e5b0a239bfa56c6eaa817
MrCoder <eagle.xiao@gmail.com>
2022-12-27 17:47:27 +1100

    Stabilize defect-406.spec.js test with waiting for an element.

--- cypress/e2e/defect-406.spec.js
+++ cypress/e2e/defect-406.spec.js
@@ -2,8 +2,8 @@
 import 'cypress-plugin-snapshots/commands';
 describe('Defect 406', function () {
   it('Fragments under Creation', function () {
-    cy.visit('http://localhost:8080/cy/defect-406-alt-under-creation.html');
-    cy.wait(1500);
+    cy.visit('http://127.0.0.1:8080/cy/defect-406-alt-under-creation.html');
+    cy.get('[data-signature="m6"]', { timeout: 5000 }).should('be.visible');
     cy.document().toMatchImageSnapshot({
       imageConfig: {"threshold": 0.01}, capture: "viewport"
     });
