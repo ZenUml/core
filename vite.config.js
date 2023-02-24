b1a661bd65a03ed167a24f95e960021d26a269c6
MrCoder <eagle.xiao@gmail.com>
2023-02-24 20:31:57 +1100

    Added git related debug information.

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,21 @@ function getCypressHtmlFiles() {
 
 const cypressHtmlFiles = getCypressHtmlFiles();
 console.log(cypressHtmlFiles);
+
+process.env.VITE_COMMIT_HASH = execSync('git rev-parse --short HEAD').toString().trim()
+process.env.VITE_GIT_BRANCH = execSync('git branch --show-current').toString().trim()
+// https://stackoverflow.com/a/45993185/529187
+process.env.VITE_GIT_TAG = execSync('git describe --tags --always --abbrev=0').toString().trim()
+console.log(`Building ${process.env.VITE_GIT_HASH} (${process.env.VITE_GIT_BRANCH}) on ${process.env.VITE_GIT_BRANCH}`)
+
+process.env.VITE_SOME_KEY = 'process.env.VITE_SOME_KEY1';
+process.env.SOME_KEY = 'process.env.VITE_SOME_KEY2';
 export default defineConfig({
+  define: {
+    'import.meta.env': {
+      VITE_SOME_KEY: JSON.stringify('import.meta.env.VITE_SOME_KEY3'),
+    },
+  },
   build: {
     rollupOptions: {
       input: ['index.html', 'embed.html', ...cypressHtmlFiles],
