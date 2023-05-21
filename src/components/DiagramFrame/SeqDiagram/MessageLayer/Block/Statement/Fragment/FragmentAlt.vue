8b7ba26957d5d8de8fee164ccaebc5c5fccdb3ba
Yanhui Li <whimet@gmail.com>
2023-05-21 20:20:27 +1000

    Collapsible if-else

--- src/components/DiagramFrame/SeqDiagram/MessageLayer/Block/Statement/Fragment/FragmentAlt.vue
+++ src/components/DiagramFrame/SeqDiagram/MessageLayer/Block/Statement/Fragment/FragmentAlt.vue
@@ -6,19 +6,29 @@
       <div
         class="header bg-skin-fragment-header text-skin-fragment-header text-base leading-4 rounded-t"
       >
-        <div class="name font-semibold p-1 border-b"><label class="p-0">Alt</label></div>
-      </div>
-      <div class="segment">
-        <div class="text-skin-fragment">
-          <label class="condition p-1">[{{ condition }}]</label>
+        <div class="name font-semibold p-1 border-b">
+          <div style="display: flex; width: 100%; justify-content: space-between;" >
+            <label class="p-0">Alt</label>
+            <div style="display: inline-block;" class="mb-2" v-b-toggle:collapse-1>
+              <svg width="24px" height="24px" class="when-open" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="miter"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><line x1="14" y1="10" x2="21" y2="3"></line><polyline points="20 10 14 10 14 4"></polyline><line x1="3" y1="21" x2="10" y2="14"></line><polyline points="4 14 10 14 10 20"></polyline></g></svg>
+              <svg width="24px" height="24px" viewBox="0 0 24 24" class="when-closed" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="expand"> <g> <polyline data-name="Right" fill="none" id="Right-2" points="3 17.3 3 21 6.7 21" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline> <line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="10" x2="3.8" y1="14" y2="20.2"></line> <line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="14" x2="20.2" y1="10" y2="3.8"></line> <polyline data-name="Right" fill="none" id="Right-3" points="21 6.7 21 3 17.3 3" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline> </g> </g> </g> </g></svg>
+            </div>
+          </div>
         </div>
-        <block
-          v-if="blockInIfBlock"
-          :style="{ paddingLeft: `${offsetX}px` }"
-          :context="blockInIfBlock"
-          :selfCallIndent="selfCallIndent"
-        ></block>
       </div>
+      
+    </div>
+    <b-collapse visible id="collapse-1" class="mt-2">
+    <div class="segment">
+      <div class="text-skin-fragment">
+        <label class="condition p-1">[{{ condition }}]</label>
+      </div>
+      <block
+        v-if="blockInIfBlock"
+        :style="{ paddingLeft: `${offsetX}px` }"
+        :context="blockInIfBlock"
+        :selfCallIndent="selfCallIndent"
+      ></block>
     </div>
     <template v-for="(elseIfBlock, index) in alt.elseIfBlock()" :key="index + 500">
       <div class="segment mt-2 border-t border-solid">
@@ -44,6 +54,7 @@
         ></block>
       </div>
     </template>
+    </b-collapse>
   </div>
 </template>
 
@@ -86,5 +97,9 @@ export default {
 /* We need to do this because tailwind 3.2.4 set border-color to #e5e7eb via '*'. */
 * {
   border-color: inherit;
+}
+.collapsed > .when-open,
+.not-collapsed > .when-closed {
+  display: none;
 }
 </style>
