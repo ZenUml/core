5b41d9d72a1c38aca54b94edb623b00005a0161b
Yanhui Li <whimet@gmail.com>
2023-05-27 10:57:26 +1000

    Extract CollapsibleButton component

--- src/components/DiagramFrame/SeqDiagram/MessageLayer/Block/Statement/Fragment/FragmentAlt.vue
+++ src/components/DiagramFrame/SeqDiagram/MessageLayer/Block/Statement/Fragment/FragmentAlt.vue
@@ -4,15 +4,11 @@
       <comment v-if="comment" :comment="comment" :commentObj="commentObj" />
 
       <div
-        class="header bg-skin-fragment-header text-skin-fragment-header text-base leading-4 rounded-t"
-      >
+        class="header bg-skin-fragment-header text-skin-fragment-header text-base leading-4 rounded-t" >
         <div class="name font-semibold p-1 border-b">
           <div style="display: flex; width: 100%; justify-content: space-between;" >
             <label class="p-0">Alt</label>
-            <div style="display: inline-block;" class="mb-2" v-b-toggle:collapse-1>
-              <svg width="24px" height="24px" class="when-open" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="miter"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><line x1="14" y1="10" x2="21" y2="3"></line><polyline points="20 10 14 10 14 4"></polyline><line x1="3" y1="21" x2="10" y2="14"></line><polyline points="4 14 10 14 10 20"></polyline></g></svg>
-              <svg width="24px" height="24px" viewBox="0 0 24 24" class="when-closed" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="expand"> <g> <polyline data-name="Right" fill="none" id="Right-2" points="3 17.3 3 21 6.7 21" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"></polyline> <line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" x1="10" x2="3.8" y1="14" y2="20.2"></line> <line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" x1="14" x2="20.2" y1="10" y2="3.8"></line> <polyline data-name="Right" fill="none" id="Right-3" points="21 6.7 21 3 17.3 3" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"></polyline> </g> </g> </g> </g></svg>
-            </div>
+            <collapsible-button />
           </div>
         </div>
       </div>
@@ -60,6 +56,7 @@
 
 <script>
 import fragment from './FragmentMixin';
+import CollapsibleButton from './CollapsibleButton.vue'
 
 export default {
   name: 'fragment-alt',
@@ -90,6 +87,7 @@ export default {
       return ctx?.braceBlock()?.block();
     },
   },
+  components: { CollapsibleButton },
 };
 </script>
 
